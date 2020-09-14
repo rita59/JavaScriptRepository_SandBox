81ad77f297e1be94911af05c73ca966f946b57db
ToshioOwa <waterblue239sky@gmail.com>
2020-09-14 11:35:18 +0900

    バージョン合わせ

--- SandBox/SSgallery/js/app.js
+++ SandBox/SSgallery/js/app.js
@@ -1,5 +1,13 @@
 // アルバムデータの作成
-// 後にfor文自動生成の形に修正
+// 後にfor文自動生成の形に修正(修正中)
+// まずファイルネームの収集。push後for文で配列に自動で入るように修正
+/* 保留　どうやらフォルダ内の一覧取得はNode.jsでないとできない可能性大　下の内容をそのままNode.jsに写す必要があるかもしれない。
+myPath = location.pathname;
+var dir_path = myPath.split("/").reverse().slice(1).reverse().join("/");
+console.log(dir_path);
+console.log(dir_path.length)
+*/
+
 let album = [
     {src: 'img/20200724-2143_537635904.jpg', msg: 'SSダミー'},
     {src: 'img/20200728-2231_538291042.jpg', msg: 'SSダミー'},
